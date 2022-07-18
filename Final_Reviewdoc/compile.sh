e9a61be15e803bc556611971d42d1228d4d565e4
Riffecs <riffecs@gmail.com>
2022-07-18 13:18:02 +0200

    push

--- Final_Reviewdoc/compile.sh
+++ Final_Reviewdoc/compile.sh
@@ -1,3 +1,3 @@
-pdflatex -synctex=1 -interaction=nonstopmode "Pflichtenheft.tex" 
-makeindex -s nomencl.ist -o Pflichtenheft.nls Pflichtenheft.nlo
-pdflatex -synctex=1 -interaction=nonstopmode "Pflichtenheft.tex" 
+pdflatex -synctex=1 -interaction=nonstopmode "Final_Reviewdoc.tex" 
+makeindex -s nomencl.ist -o Final_Reviewdoc.nls Final_Reviewdoc.nlo
+pdflatex -synctex=1 -interaction=nonstopmode "Final_Reviewdoc.tex" 
